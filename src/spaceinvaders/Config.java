b055270c1be42958ac318108bec2513f6f293652
Alex Petenchea <alex.petenchea@gmail.com>
2017-02-24 14:26:03 +0200

    Added comments and renamed configuration files.

--- src/spaceinvaders/Config.java
+++ src/spaceinvaders/Config.java
@@ -1,12 +1,21 @@
 package spaceinvaders;
 
+/**
+ * Main configuraion.
+ *
+ * <p>Used to retrive the path to configuration files
+ */
 public class Config {
   private static final String CONFIG_PATH = "../config/";
   private static Config singleton;
 
-
   private Config() {}
 
+  /**
+   * Get the single instance of this class.
+   *
+   * <p>If an instance does not exist, one is created automatically.
+   */
   public static Config getInstance() {
     if (singleton == null) {
       singleton = new Config();
@@ -15,14 +24,14 @@ public class Config {
   }
 
   public String getGameConfigFile() {
-    return CONFIG_PATH + "GameConfig.json";
+    return CONFIG_PATH + "game.json";
   }
 
   public String getResourcesConfigFile() {
-    return CONFIG_PATH + "ResourcesConfig.json";
+    return CONFIG_PATH + "resources.json";
   }
 
   public String getClientConfigFile() {
-    return CONFIG_PATH + "ClientConfig.json";
+    return CONFIG_PATH + "client.json";
   }
 }
